adeb8688a4d4673ff977a293ef4568ae1f64c14d
Yasuhiro Usutani <y_usutani@me.com>
2011-08-27 19:08:15 +0900

    add usage

--- cutter.rb
+++ cutter.rb
@@ -1,14 +1,19 @@
 require 'optparse'
 
+def usage
+  puts "Usage: ruby cutter.rb [--dst DST_PREFIX] <file(s)>"; exit
+end
+
 dst_prefix = "_"
 
 args = ARGV.dup
 opts_parser = OptionParser.new do |opts|
-  opts.on('--dst DST_PREFIX') { |opt| dst_prefix = opt }
+  opts.on("--dst DST_PREFIX") { |opt| dst_prefix = opt }
 end
 args = opts_parser.parse!(args)
 
 src_files = args.shift
+usage unless src_files
 
 Dir.glob(src_files) do |src_file|
   dst_file = dst_prefix + src_file
